99db4bad022b43b60fb6bc13004ca6eabf7f9fb7
zainabnoori <zainab.noori@and.digital>
2023-04-04 15:38:07 +0100

    Solved third challenge

--- src/03-form-validator/FormValidator.js
+++ src/03-form-validator/FormValidator.js
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 
-export default function FormValidator () {
+export default function FormValidator() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
 
+  const handleSubmission = () => {
+    if (email === "" || password === "" || passwordConfirm === "") {
+      alert("please, enter a value");
+    }
+    else {
+      alert("user created!");
+    }
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmission}>
       <h2>Sign Up!</h2>
       <label htmlFor='email'>Email</label>
       <input
